bc78b8061b016a1ebd9112503bf47f05206ac606
SeptAlfauzan <alfauzansepta@gmail.com>
2022-01-31 14:28:57 +0700

    initial commit

---
+++ components/homebanner.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useRef } from "react";
+import Image from "next/image";
+import MainLogo from '../public/logo-brainover.svg'
+import { gsap } from "gsap";
+
+
+const HomeBanner = () => {
+    const headerTopRef = useRef();
+    const headerBottomRef = useRef();
+    const colorGradient = "linear-gradient(90deg, rgba(29,0,36,1) 0%, rgba(73,9,121,1) 35%, rgba(0,212,255,1) 100%);"
+    useEffect(()=>{
+      // gsap.to(headerTopRef.current, {color: "#121212", repeat: -1, duration: 3})
+      // .to(headerBottomRef.current, {color: "#121212", repeat: -1, duration: 3})
+    },[])
+
+    return (
+        <div className='w-full h-screen flex justify-center items-center flex-col'>
+          <div className='relative w-2/4 h-2/4 flex flex-col justify-center items-center md:mt-0 -mt-20'>
+            <div className='md:w-96 md:h-96 w-72 h-72 relative bg-blue-50 p-10'>
+              <Image src={MainLogo} alt='Brain.over logo' layout='fill'/>
+            </div>
+            <div className='absolute md:-left-10 md:top-5 -left-20 -top-1'>
+              <h3 className='text-8xl md:text-9xl' ref={headerTopRef}>Brain</h3>
+            </div>
+            <div className='absolute md:right-0 md:-bottom-10 -bottom-1 -right-20'>
+              <h3 className='text-8xl md:text-9xl' ref={headerBottomRef}>Over</h3>
+            </div>
+          </div>
+        </div>
+    )
+}
+export default HomeBanner;
---
+++ components/navbar.js
@@ -0,0 +1,41 @@
+import Link from "next/link";
+import React from "react";
+import {MenuOutline, CloseOutline} from 'react-ionicons';
+
+
+const Navbar = () => {
+    const [active, setActive] = React.useState(false)
+
+    const handleOpenNavbar = () => setActive(!active)
+    
+    return(
+        <>
+            <nav className={`md:visible ${active? '' : '-translate-x-full'} md:translate-x-0 transition-all duration-200 w-full md:fixed absolute bg-blue-50 md:bg-white md:h-auto h-screen z-20 `}>
+                <ul className="flex md:flex-row flex-col gap-2 px-10 py-5 justify-center items-center h-full md:h-auto">
+                    <li  className="text-xl">
+                        <Link href='#'>Home</Link>
+                    </li>
+                    <div className="md:ml-auto flex md:flex-row flex-col gap-2 text-center">
+                        <li  className="text-xl">
+                            <Link href='#'>About</Link>
+                        </li>
+                        <li  className="text-xl">
+                            <Link href='#'>Work</Link>
+                        </li>
+                        <li  className="text-xl">
+                            <Link href='#'>Contact Us</Link>
+                        </li>
+                    </div>
+                <button className="visible md:invisible fixed top-10 right-10" onClick={handleOpenNavbar}>
+                    <CloseOutline width={'50px'} height={'50px'}/>
+                </button>
+                </ul>
+            </nav>
+            <button className="block md:hidden fixed top-5 ml-4 bg-white shadow-lg z-10 rounded" onClick={handleOpenNavbar}>
+                <MenuOutline width={'50px'} height={'50px'}/>
+            </button>
+        </>
+    )
+}
+
+export default Navbar;
